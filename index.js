08a30703ee4c718430afea7e32780538fcc7c2ed
pedrotelefonica <pedro.borrego@alten.es>
2021-06-23 14:35:42 +0200

    add timestamp to logs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.on('connection', function (socket) {
   // receiving messages
   socket.on('message', function (msg) {
     //sockets.forEach((s) => s.send(msg));
-    console.log(msg);
+    console.log(getFullTimestamp() + ' - ' + msg);
   });
 
   // When a socket closes, or disconnects, remove it from the array.
@@ -20,3 +20,9 @@ server.on('connection', function (socket) {
     sockets = sockets.filter((s) => s !== socket);
   });
 });
+
+function getFullTimestamp () {
+  const pad = (n,s=2) => (`${new Array(s).fill(0)}${n}`).slice(-s);
+  const d = new Date();
+  return `${pad(d.getFullYear(),4)}-${pad(d.getMonth()+1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}.${pad(d.getMilliseconds(),3)}`;
+}
